438e8139aeaeb95a368260fe00b44fc0c7116e06
zc <zouchao0911@gmail.com>
2019-09-11 10:45:55 +0800

    Create WangComponent.ts

---
+++ src/WangComponent.ts
@@ -0,0 +1,69 @@
+import Component from './Component'
+require('./assets/wang2.gif')
+require('./assets/wang1.gif')
+export default class WangComponent extends Component {
+  constructor() {
+    super('wang-component', {enableResize: false})
+    let content = `
+        <a class="wang-box"><img src="./assets/wang2.gif"><span>璇子</span></a>
+      `
+    this.$contentBox.append(content)
+  }
+  getProps() {
+    return {}
+  }
+  toHtml() {
+    return ''
+  }
+  initPorpPanel() {
+  }
+  openEditDialog() {
+    let layer = layui.layer;
+    let $layerElem = null;
+    let layerNo = layer.open({
+      type: 1,
+      title: '旺旺设置',
+      skin: 'layui-layer-rim', //加上边框
+      area: ['600px', '600px'],
+      success: function(layerElem, index) {
+        $layerElem = $(layerElem)
+        $layerElem.find('.cancel-btn').on('click', function() {
+          layer.close(index)
+        })
+      },
+      content: `<form class="layui-form" lay-filter="imgComponentForm">
+        <div class="layui-tab layui-tab-brief">
+          <ul class="layui-tab-title">
+            <li class="layui-this">内容设置</li>
+             <li>边框设置</li>
+             <li>阴影外框</li>
+            <li>动画设置</li>
+          </ul>
+          <div class="layui-tab-content">
+            <div class="layui-tab-item layui-show">
+            </div>
+            <div class="layui-tab-item">
+            </div>
+            <div class="layui-tab-item">
+            </div>
+          </div>
+        </div>
+        <div class="layui-form-item">
+          <div class="layui-ft-btn">
+            <button class="layui-btn" lay-submit lay-filter="wangComponentForm">确定</button>
+            <button type="button" class="cancel-btn layui-btn layui-btn-primary">取消</button>
+          </div>
+        </div>
+      </form>`
+    });
+    var form = layui.form
+    form.render();
+    form.on('submit(wangComponentForm)', function(data) {
+      layer.close(layerNo)
+      return false; //阻止表单跳转。如果需要表单跳转，去掉这段即可。
+    });
+  }
+  update(formData) {
+
+  }
+}
